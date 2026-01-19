8a43142b196b850cc8770497e31a20701d275210
CHIRAYU-365 <chirayujain362005@gmail.com>
2026-01-19 10:16:08 +0530

    LEC2

---
+++ relational.java
@@ -0,0 +1,12 @@
+public class relational {
+    public static void main(String[] args) {
+        int a = 10;
+        float b =10.0f;
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+    }
+}
