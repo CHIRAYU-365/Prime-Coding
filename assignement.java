b43dbc53ebd3038c2450e6b04b137b8a75c57f8d
CHIRAYU-365 <chirayujain362005@gmail.com>
2026-01-19 10:21:01 +0530

    lec2

---
+++ assignement.java
@@ -0,0 +1,13 @@
+public class assignement {
+    public static void main(String[] args) {
+        int a=50;
+        int b=5;
+        System.out.println(b=a);
+        System.out.println(b+=a);
+        System.out.println(b-=a);
+        System.out.println(b*=a);
+        System.out.println(b/=a);
+        System.out.println(b%=a);
+    }
+    
+}
