fca4c9d1328076c49ff4bd93ac826c9ca8cd0618
CHIRAYU-365 <chirayujain362005@gmail.com>
2026-01-23 08:37:14 +0530

    lec4.9

--- PATTERNS/pattern10.java
+++ PATTERNS/pattern10.java
@@ -1,12 +1,10 @@
 package PATTERNS;
 public class pattern10 {
     public static void main(String[] args) {
-        
         int n=5;
         int r=1;
         int s=n*2-1;
         int t=0;
-
         while (r<=n){
             int i=1;
             while (i<=t){
