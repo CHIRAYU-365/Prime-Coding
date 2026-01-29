725d312b8d8763cb0670c2a0636466af35dc2a13
CHIRAYU-365 <chirayujain362005@gmail.com>
2026-01-29 09:33:17 +0530

    lec9.3

--- fibonacci.java
+++ fibonacci.java
@@ -1,3 +1,12 @@
 public class fibonacci {
-    
+    public static void main(String[] args) {
+        int a=0;
+        int b=1;
+        for (int i=1;i<=6;i++){
+            int c =a+b;
+            a=b;
+            b=c;
+        }
+        System.out.println(a);
+    }
 }
