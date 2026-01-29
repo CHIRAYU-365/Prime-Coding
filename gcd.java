1428f1cf5920bf5f4b5c3dda6e89fecff3ab8668
CHIRAYU-365 <chirayujain362005@gmail.com>
2026-01-29 09:52:44 +0530

    lec9.4

---
+++ gcd.java
@@ -0,0 +1,31 @@
+public class gcd {
+    public static void main(String[] args) {
+        int a=36;
+        int b=60;
+        int d=0;
+        if (a>b){
+            for(int i=1;i<=(b);i++){
+                if(a%i==0 && b%i==0){
+                    d=i;
+                    continue;
+                }
+                else{
+                    continue;
+                }
+            }
+            System.out.println(d);
+        }
+        else{
+            for(int i=1;i<=(a);i++){
+                if(a%i==0&&b%i==0){
+                    d=i;
+                    continue;
+                }
+                else{
+                    continue;
+                }
+            }
+            System.out.println(d);
+        }
+    }
+}
