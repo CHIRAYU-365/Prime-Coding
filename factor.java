58fac63fc7cb9ba80f1afc8f16149ee4384b8319
CHIRAYU-365 <chirayujain362005@gmail.com>
2026-01-30 09:34:16 +0530

    lec9.4

---
+++ factor.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class factor {
+    public static void main(String[] args) {
+        int n = 100;
+        List<Integer> s = new ArrayList<>();
+        for(int i=2;i<=n;i++){
+            while (n%i==0 && n>0){
+                
+                s.add(i);
+                n=n / i ;
+                }
+        }
+        Set<Integer> t = new HashSet<>(s);
+        System.out.println(t);
+
+    }
+}
