a98acf8548b90b41af5c946376e90df8aa7d366f
CHIRAYU-365 <chirayujain362005@gmail.com>
2026-02-02 09:41:18 +0530

    lec10.1

---
+++ number_system.java
@@ -0,0 +1,19 @@
+public class number_system{
+    public static void main(String[] args) {
+        int n= 4;
+        int t = 0;
+        int r =0;
+        while (n>0){
+            t += n%2;
+            t*=10;
+            n/=2;
+        }
+        while (t>0){
+            
+            r+=t%10;
+            r*=10;
+            t/=10;
+        }
+        System.out.println(r/10);
+    }
+}
