f8ab80f61908897fe233fe03fdede5da85d72605
CHIRAYU-365 <chirayujain362005@gmail.com>
2026-02-12 09:56:02 +0530

    lec17.2

---
+++ max_arr.java
@@ -0,0 +1,16 @@
+public class max_arr {
+    public static void main(String[] args) {
+        int[] arr = new int[]{4,2,8,3,5,0};
+        System.out.print(MAX(arr));
+    }
+    public static int MAX(int[] arr) {
+        int max=arr[0];
+        for(int i=1;i<arr.length;i++){
+            if (arr[i] > max){
+                max = arr[i];
+            }
+        }
+        return max;
+        
+    }
+}
