0484ad8fb7aa29d77116eb68136a8da6519c396b
CHIRAYU-365 <chirayujain362005@gmail.com>
2026-02-12 09:36:57 +0530

    lec17.1

--- Array.java
+++ Array.java
@@ -1,5 +1,17 @@
 public class Array {
     public static void main(String[] args) {
-        
+        int[] arr = new int[]{4,2,8,3,5,0};
+        int target =5;
+        System.out.println("the target is at indexes :");
+        find(arr,target);
     }
+    public static int find(int[] arr,int target) {
+        for(int i=0;i<arr.length;i++){
+            if (arr[i] == target){
+                System.out.println(i);
+            }
+        }
+        return -1;
+    }
+    
 }
